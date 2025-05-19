12457b2771cec39205a1f1550c307523236f8229
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2025-05-19 10:58:12 +0900

    add typespec

--- README.md
+++ README.md
@@ -8,7 +8,17 @@ npm create vite@latest
 ### tailwindcss
 npm install tailwindcss @tailwindcss/vite
 
-
+### 実行
+```
+cd development
+docker compose exec frontend bash
+npm run dev
+```
 
 ## backend
+### 実行
+```
+cd development
+docker compose exec api bash
 bacon run-long
+```
---
+++ docs/typespec/move.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+function copy_move() {
+    if [ ! -d $2 ]; then
+        mkdir $2
+    else
+        rm -rf $2
+        mkdir $2
+    fi
+    cp -r $1/* $2
+}
+
+SRCDIR=./tsp-output/clients/js/src/api
+DSTDIR=../../apps/frontend/src/api
+copy_move $SRCDIR $DSTDIR
+
+SRCDIR=./tsp-output/clients/js/src/helpers
+DSTDIR=../../apps/frontend/src/helpers
+copy_move $SRCDIR $DSTDIR
+
+SRCDIR=./tsp-output/clients/js/src/models
+DSTDIR=../../apps/frontend/src/models
+copy_move $SRCDIR $DSTDIR
+
